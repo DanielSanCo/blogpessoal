8928375fde0bf85ce06163cd0f5046aececd6e7a
DanielSanCo <daniel.santos3210@outlook.com>
2022-11-08 11:11:52 -0300

    criando render 2

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
